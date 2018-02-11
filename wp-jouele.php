0591d251dc7ace7a49f4153fb5ee5140cdc57edc
Alex Buznik <shu@buznik.net>
2018-02-12 01:17:41 +0200

    Upgrade Jouele to 3.0.4; Fixes #2

--- wp-jouele.php
+++ wp-jouele.php
@@ -5,7 +5,7 @@
 /*
 Plugin Name: Jouele for Wordpress
 Plugin URI: https://github.com/beshur/wp-jouele
-Version: 0.1.6
+Version: 0.2.0
 Description: Insert audio files with a great Jouele player. Jouele player by Ilya Birman (https://github.com/ilyabirman/Jouele).
 Author: alexbuznik
 Author URI: http://buznik.com/
@@ -17,10 +17,9 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 function load_static() {
 
-	wp_enqueue_script( 'jplayer', plugins_url( 'jouele/jquery.jplayer.min.js', __FILE__ ), array('jquery',), '2.1.0', true );
-	wp_enqueue_script( 'jouele', plugins_url( 'jouele/jouele.js', __FILE__ ), array('jquery',), null, true );
+	wp_enqueue_script( 'jouele', plugins_url( 'jouele/jouele.min.js', __FILE__ ), array('jquery',), null, true );
 
-	wp_enqueue_style( 'jouele', plugins_url( 'jouele/jouele.css', __FILE__ ), null, null );
+	wp_enqueue_style( 'jouele', plugins_url( 'jouele/jouele.min.css', __FILE__ ), null, null );
 
 }
 add_action( 'init', 'load_static' );
